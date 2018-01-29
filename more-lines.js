c5092e2441f1dc21134f837a7b497b325989b431
Tina Ye <tinaye@berkeley.edu>
2018-01-29 13:40:33 -0800

    Create more-lines.js

---
+++ more-lines.js
@@ -0,0 +1,25 @@
+a: lines start from center
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+  line(170, 170, random(width), random(height));
+}
+
+
+b: lines end in top right quadrant
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+  line(random(width), random(height), random(170, 340), random (0, 170));
+}
+
+
+
+
+
